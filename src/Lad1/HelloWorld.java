97787b9ab268cea5dcac1109db2c7a6991e9ae61
janny <missnuttiya.p@rmutsvmail.com>
2020-07-15 14:56:18 +0700

    add more code

--- src/Lad1/HelloWorld.java
+++ src/Lad1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World !!!");
+        System.out.println("Nuttiya Phonlamuang");
+        System.out.println("363211760039");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
 
 
 
