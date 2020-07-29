f3e393a8b9d699408753d6280f31f03df6a3ccc3
jane <missnuttiya@rmtsvmail.com>
2020-07-29 15:56:15 +0700

    Data and Basic input

---
+++ src/Exercise/Chapter4_1.java
@@ -0,0 +1,9 @@
+package Exercise;
+
+public class Chapter4_1 {
+
+    public static void main(String[] args) {
+
+
+    }//main
+}//class
---
+++ src/Lad2/Basiclnput.java
@@ -0,0 +1,31 @@
+package Lad2;
+
+import java.util.Scanner;
+
+public class Basiclnput {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter you name ?: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Your name is "+name);
+
+
+        System.out.println("อายุเท่าไร ?: ");
+        int age = scanner.nextInt();
+        System.out.println("อายุของคุณคือ " + age);
+
+        System.out.println("Enter you nick name ?: ");
+        String nickName = scanner.nextLine();
+
+
+
+    }//main
+
+
+
+
+}//class
