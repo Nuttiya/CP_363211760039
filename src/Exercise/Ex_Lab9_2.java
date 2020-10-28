5e26c7decf1f296692de55737306c876949cbd62
STP_COM08 <missnuttiya.p@rmutsvmail.com>
2020-10-28 15:28:00 +0700

    Lab9

--- src/Exercise/Ex_Lab9_2.java
+++ src/Exercise/Ex_Lab9_2.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-public class Ex_lab9_2 {
+public class Ex_Lab9_2 {
     public static void main(String[] args) {
         Path pFile = Paths.get("Programing.txt");
         try {
@@ -32,7 +32,9 @@ public class Ex_lab9_2 {
             writer.write("5+5=10");
             writer.newLine();
             writer.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+}
